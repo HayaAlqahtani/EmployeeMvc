a7f0d52f77f239e7e10a9d7e54554a7e1b4a2173
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-04-16 14:36:16 +0300

    Add project files.

---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Employee> list = new List<Employee>
+           {
+                new Employee {EmployeeId  = 1, FirstName ="haya", LastName="alqahtani", Department= "ce", Email="haya@email" },
+                new Employee {EmployeeId  = 2, FirstName ="amar", LastName="alfailakawi", Department= "ce", Email="amar@email" }
+
+
+           };
+            ViewData["data"]=list;
+            return View();
+        }
+    }
+}
